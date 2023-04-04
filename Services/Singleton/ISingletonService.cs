9d866e17d08c23ca03fd3b09dd2735cc1be0a9a2
wyatth7 <whardin1010@gmail.com>
2023-04-04 16:57:02 -0400

    Added inline documentation

--- Services/Singleton/ISingletonService.cs
+++ Services/Singleton/ISingletonService.cs
@@ -2,10 +2,26 @@
 
 namespace PremiumTravelService.Api.Services.Singleton;
 
+/// <summary>
+/// Gets instances of singletons
+/// </summary>
 public interface ISingletonService
 {
+    /// <summary>
+    /// Gets agent singleton
+    /// </summary>
+    /// <returns>Instance of agent singleton</returns>
     AgentSingleton GetAgentSingleton();
+    
+    /// <summary>
+    /// Gets traveller singleton
+    /// </summary>
+    /// <returns>Instance of traveller singleton</returns>
     TravellerSingleton GetTravellerSingleton();
     
+    /// <summary>
+    /// Gets package singleton
+    /// </summary>
+    /// <returns>Instance of package singleton</returns>
     PackageSingleton GetPackageSingleton();
 }
--- Singleton/ISingleton.cs
+++ Singleton/ISingleton.cs
@@ -1,7 +1,23 @@
 ﻿namespace PremiumTravelService.Api.Singleton;
 
+/// <summary>
+/// Singleton interface for use on all singleton objects
+/// </summary>
+/// <typeparam name="TSingleton">Singleton type</typeparam>
+/// <typeparam name="TData">Data type to be returned</typeparam>
 public interface ISingleton<out TSingleton, TData>
 {
+    /// <summary>
+    /// Gets instance of singleton
+    /// </summary>
+    /// <returns>singleton instance</returns>
+    /// <exception cref="NotImplementedException">Throws if not implemented</exception>
     static TSingleton GetInstance() => throw new NotImplementedException();
-    static Task<IReadOnlyCollection<TData>> GetData() => throw new NotImplementedException();
+    
+    /// <summary>
+    /// Gets data based on data type generic
+    /// </summary>
+    /// <returns>Returns readonly collection of data</returns>
+    /// <exception cref="NotImplementedException">Throws if not implemented</exception>
+    Task<IReadOnlyCollection<TData>> GetData() => throw new NotImplementedException();
 }
