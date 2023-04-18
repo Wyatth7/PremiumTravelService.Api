94c7c8b2c50e4c3b919f587d0543d605dd923771
Wyatt <wyatt@highmatch.com>
2023-04-17 23:07:27 -0400

    Added Nunit to project

---
+++ NunitTests/TestingNunit.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace PremiumTravelService.Api.NunitTests;
+
+[TestFixture]
+public class TestingNunit
+{
+    [Test]
+    public void SampleTest()
+    {
+        var val1 = 5;
+        var val2 = 5;
+        
+        Assert.AreEqual(val1, val2);
+    }
+}
