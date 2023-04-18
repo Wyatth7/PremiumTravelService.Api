25dd22a9f6e161354f0b136fdb1f86d6c547a2fe
Wyatt <wyatt@highmatch.com>
2023-04-18 01:11:23 -0400

    Fixed data storage bug

--- DataStorage/FileHandling/JsonFileHandler.cs
+++ DataStorage/FileHandling/JsonFileHandler.cs
@@ -11,18 +11,57 @@ namespace PremiumTravelService.Api.DataStorage.FileHandling;
 public class JsonFileHandler : FileHandler
 {
     public override async Task Write(StorageData payload, string path) {
-        await File.WriteAllTextAsync(path, "");
-        var options = new JsonSerializerOptions {WriteIndented = true};
-        await using var writeStream = File.Open(path, FileMode.Open, FileAccess.Write, FileShare.ReadWrite);
-        await JsonSerializer.SerializeAsync(writeStream, payload, options).ConfigureAwait(false); ;
-        await writeStream.DisposeAsync();
+        while (IsFileLocked(new FileInfo(path)))
+        {
+            Console.WriteLine("waiting");
+        }
+        
+        try
+        {
+            await File.WriteAllTextAsync(path, "");
+            var options = new JsonSerializerOptions {WriteIndented = true};
+            await using var writeStream = File.Open(path, FileMode.Open, FileAccess.Write, FileShare.Write);
+            await JsonSerializer.SerializeAsync(writeStream, payload, options).ConfigureAwait(false); ;
+            await writeStream.DisposeAsync();
+
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("catch");
+            throw;
+        }
     }
 
     public override async Task<StorageData> Read(string path) {
-        await using var readStream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        while (IsFileLocked(new FileInfo(path)))
+        {
+            Console.WriteLine("waiting");
+        }
+        
+        await using var readStream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
         var data = await JsonSerializer.DeserializeAsync<StorageData>(readStream).ConfigureAwait(false);
 
         await readStream.DisposeAsync();
         return data ?? new StorageData();
+    }
+    
+   public static bool IsFileLocked(FileInfo file)
+    {
+        FileStream stream = null;
+
+        try
+        {
+            stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+        }
+        catch (IOException)
+        {
+            return true;
+        }
+        finally
+        {
+            if (stream != null)
+                stream.Close();
+        }
+        return false;
     }
 }
