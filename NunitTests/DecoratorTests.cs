cf81366255d5efa1a6e7dcac9d924f497602f03f
Nicholas Grimaldi <nicholasgrimaldi42@gmail.com>
2023-04-19 20:50:52 -0400

    added file for each pattern

---
+++ NunitTests/DecoratorTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace PremiumTravelService.Api.NunitTests;
+
+[TestFixture]
+public class DecoratorTests
+{
+    [Test]
+    public void SampleTest()
+    {
+        var val1 = 6;
+        var val2 = 6;
+
+        Assert.AreEqual(val1, val2);
+    }
+}
